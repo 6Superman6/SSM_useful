d5d62cc193179dfd0f0fe16e46b4335b081d7215
6Superman6 <465445097@qq.com>
2019-10-29 14:42:30 +0800

    one time

---
+++ ssm_01/src/main/java/demo/controller/MyExceptionHandler.java
@@ -0,0 +1,26 @@
+package demo.controller;
+
+import demo.utils.ServerResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice  // 异常处理器
+public class MyExceptionHandler {
+
+//    @ResponseBody
+//    @ExceptionHandler    // 如果后边不跟(*****.class) 默认是对所有的异常进行处理
+////    @ExceptionHandler(UserNotExistException.class)  //支队UserNotExistException异常进行处理
+//    public Map<String,Object> handlerException(Exception e)
+//    {
+//        Map<String,Object> map = new HashMap<>();
+//        map.put("status","-1");
+//        map.put("msg","发生未知错误");
+//        return map;
+//    }
+
+}
---
+++ ssm_01/src/main/java/demo/controller/UserController.java
@@ -0,0 +1,66 @@
+package demo.controller;
+
+import com.sun.security.ntlm.Server;
+import demo.model.User;
+import demo.service.UserService;
+import demo.utils.ServerResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(path = "/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // 添加数据
+    @RequestMapping(path = "/add",method = RequestMethod.POST)
+    public ServerResponse add(@Valid User user)
+    {
+        return userService.insert(user);
+    }
+
+    // 删除数据
+    @RequestMapping(path = "/del/{id}",method = RequestMethod.DELETE)
+    public ServerResponse del(@PathVariable("id") int id)
+    {
+        return userService.delete(id);
+    }
+
+    // 修改数据
+    @RequestMapping(path = "/do",method = RequestMethod.POST)
+    public ServerResponse update(@Valid User user)
+    {
+        return userService.update(user);
+    }
+
+    // 获取数据--通过编号
+    @RequestMapping(path = "/do/{id}",method = RequestMethod.GET)
+    public ServerResponse get(@PathVariable("id") int id)
+    {
+        return userService.get(id);
+    }
+
+    // 获取全部数据
+    @RequestMapping(path = "/do",method = RequestMethod.GET)
+    public ServerResponse findAll()
+    {
+        return userService.findAll();
+    }
+
+    // 获取数据--通过用户名
+    @RequestMapping(path = "/name/{name}",method = RequestMethod.GET)
+    public ServerResponse getByName(@PathVariable("name") String name)
+    {
+        return userService.getyName(name);
+    }
+
+
+
+}
